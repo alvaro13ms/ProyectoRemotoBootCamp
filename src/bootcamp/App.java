c8f6d9e1e2f7712e1249c74ca61695a03f2ea045
alvaro13ms <alvaro13ms@gmail.com>
2022-01-26 08:52:34 +0100

    Commit sobre rama desaMarzo

--- src/bootcamp/App.java
+++ src/bootcamp/App.java
@@ -7,9 +7,12 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola Febrero");
-		
-		for(int i=0;i<=10;i++) {
+		System.out.println("Hola Marzo");
+
+		for(int i=0;i<=30;i++) {
 			System.out.println("Empezamos a la de " + i);
 		}
+		System.out.println("Hola NTTData");
+
 	}
 }
