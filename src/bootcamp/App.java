c83fa7ccac5793a698912dcf1459d4afc005dc65
alvaro13ms <alvaro13ms@gmail.com>
2022-01-26 08:17:39 +0100

    Commit sobre la rama desaFebrero

--- src/bootcamp/App.java
+++ src/bootcamp/App.java
@@ -6,5 +6,10 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("Hola Febrero");
+		
+		for(int i=0;i<=10;i++) {
+			System.out.println("Empezamos a la de " + i);
+		}
 	}
 }
