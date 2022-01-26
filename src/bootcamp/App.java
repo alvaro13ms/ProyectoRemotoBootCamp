e836f7d10c65f6a7e9e320a4e6802bb016263193
alvaro13ms <alvaro13ms@gmail.com>
2022-01-26 08:51:12 +0100

    Commit sobre rama master

--- src/bootcamp/App.java
+++ src/bootcamp/App.java
@@ -1,15 +1,17 @@
 package bootcamp;
 
-
-
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola Febrero");
-		
-		for(int i=0;i<=10;i++) {
+
+		for (int i = 0; i <= 20; i++) {
 			System.out.println("Empezamos a la de " + i);
+			System.out.println("Empezamos a la de " + i);
+
 		}
+
+		System.out.println("Hola master");
 	}
 }
